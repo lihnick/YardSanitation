4da252dcd956ee840a294c222456ebe3e156abcf
Nick Li <hli109@hawk.iit.edu>
2017-11-12 02:12:24 -0600

    added markers and popup image

--- js/app.js
+++ js/app.js
@@ -11,16 +11,15 @@ firebase.initializeApp(config);
 
 let ref = firebase.database().ref("users");
 
-let data = {};
+let data = [];
 let map;
-var markers = new Array();
 
 ref.once('value', function(snapshot) {
 	console.log(snapshot.val());
 	for (var user in snapshot.val()) {
 		var posts = snapshot.val()[user];
 
-		data[user] = []; 
+		// data[user] = []; 
 		var first = posts.first_name;
 		var last = posts.last_name;
 		
@@ -28,7 +27,8 @@ ref.once('value', function(snapshot) {
 			var entry = posts.posts[time];
 			// console.log(time);
 			if (entry.url){
-				data[user].push({
+				data.push({
+					"user_id": user,
 					"post_id": time,
 					"first_name": first,
 					"last_name": last,
@@ -38,7 +38,8 @@ ref.once('value', function(snapshot) {
 					"lng": entry.lng
 				});
 			} else {
-				data[user].push({
+				data.push({
+					"user_id": user,
 					"post_id": time,
 					"first_name": first,
 					"last_name": last,
@@ -49,6 +50,39 @@ ref.once('value', function(snapshot) {
 			}
 		}
 	}
+}).then(function(res) {
+	console.log(res.val());
+
+	var bounds = new google.maps.LatLngBounds();
+	var infoWindow = new google.maps.InfoWindow(), marker;
+
+	for (var i = 0; i < data.length; i++) {
+		console.log(data[i]);
+		var position = new google.maps.LatLng(data[i].lat, data[i].lng);
+		bounds.extend(position);
+		marker = new google.maps.Marker({
+			position: position,
+			map: map,
+			title: data[i].first_name
+		});
+
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+				if (data[i].img_url) {
+					infoWindow.setContent("<img src='"+data[i].img_url+"' height=100px width=100px>");
+					infoWindow.open(map, marker);
+				}
+			}
+		}) (marker, i));
+
+		map.fitBounds(bounds);
+	}
+
+	// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(9);
+        google.maps.event.removeListener(boundsListener);
+    });
 });
 
 function initMap() {
