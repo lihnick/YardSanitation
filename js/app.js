c81ee1f10f807a639fb6cafc4c97e4af6aa4366c
Nick Li <hli109@hawk.iit.edu>
2017-11-12 12:04:30 -0600

    update marker popup

--- js/app.js
+++ js/app.js
@@ -85,6 +85,48 @@ ref.once('value', function(snapshot) {
     });
 });
 
+ref.on('value', function(snapshot) {
+	var added = [];
+	// console.log(snapshot.val());
+	for (var user in snapshot.val()) {
+		var posts = snapshot.val()[user];
+
+		var first = posts.first_name;
+		var last = posts.last_name;
+		
+		for (var time in posts.posts) {
+			var entry = posts.posts[time];
+			// console.log(time);
+			if (entry.url){
+				added.push({
+					"user_id": user,
+					"post_id": time,
+					"first_name": first,
+					"last_name": last,
+					"timestamp": entry.timestamp,
+					"img_url": entry.url,
+					"lat": entry.lat,
+					"lng": entry.lng
+				});
+			} else {
+				added.push({
+					"user_id": user,
+					"post_id": time,
+					"first_name": first,
+					"last_name": last,
+					"timestamp": entry.timestamp,
+					"lat": entry.lat,
+					"lng": entry.lng
+				});
+			}
+		}
+	}
+	arraydiff(data, added);
+})
+// .then(function(res) {
+// 	console.log(res);
+// });
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 41.878159, lng: -87.629732},
@@ -92,6 +134,61 @@ function initMap() {
 	});
 }
 
+function compare(a, b) {
+	if (a && a.timestamp && b && b.timestamp) {
+		if (a.timestamp < b.timestamp) {
+			return -1;
+		}
+		if (a.timestamp > b.timestamp) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+function arraydiff(a, b) {
+	var old = a.sort(compare);
+	var diff = b.sort(compare);
+	var res = diff.splice(0, old.length);
+	if (res.length == 0) {
+		return [];
+	}
+	else {
+		console.log(res);
+		data.push(res[0]);
+
+		var bounds = new google.maps.LatLngBounds();
+		var infoWindow = new google.maps.InfoWindow(), marker;
+
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i]);
+			var position = new google.maps.LatLng(res[i].lat, res[i].lng);
+			bounds.extend(position);
+			marker = new google.maps.Marker({
+				position: position,
+				map: map,
+				title: res[i].first_name
+			});
+
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+					if (res[i].img_url) {
+						infoWindow.setContent("<img src='"+res[i].img_url+"' height=100px width=100px>");
+						infoWindow.open(map, marker);
+					}
+				}
+			}) (marker, i));
+
+			// map.fitBounds(bounds);
+		}
+
+		// Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+	    // var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+	    //     //this.setZoom(9);
+	        // google.maps.event.removeListener(boundsListener);
+	    // });
+	}
+}
 
 
 
