f5c959406e752ddacdeeaae77e687a944de82ddd
Nick Li <hli109@hawk.iit.edu>
2017-11-12 00:08:38 -0600

    initial files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDE-oe6jwDbtq1S6fx-N5_DG3AFBjTinss",
+	authDomain: "wild-fc8bb.firebaseapp.com",
+	databaseURL: "https://wild-fc8bb.firebaseio.com",
+	projectId: "wild-fc8bb",
+	storageBucket: "wild-fc8bb.appspot.com",
+	messagingSenderId: "299838869757"
+};
+firebase.initializeApp(config);
